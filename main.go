95443d86e682f7a159b7cfae0fb90a33c6c9a5bc
Nurcahyo al Hidayah <2light.hidayah@gmail.com>
2018-11-06 04:14:06 +0700

    Improve executable flags and add exite status code

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
 	"fmt"
+	"os"
 	"strings"
+	"time"
 
 	ps "cloud.google.com/go/pubsub"
 )
@@ -13,53 +16,121 @@ var (
 	proj string
 )
 
-func GetClient() (*ps.Client, context.Context) {
+func GetClient(ctx context.Context) *ps.Client {
 
 	if proj == "" {
 		panic("Can't find Google cloud project used for pubsub.")
 	}
-
-	ctx := context.Background()
 	c, err := ps.NewClient(ctx, proj)
+
 	if err != nil {
 		panic(err)
 	}
-	return c, ctx
+	return c
 }
 
-func subscribe(subscription string, ch chan []byte) {
+func subscribe(ctx context.Context, subscription string, ch chan *ps.Message) {
 
-	client, ctx := GetClient()
+	client := GetClient(ctx)
 	sub := client.Subscription(subscription)
 
-	cctx, cancel := context.WithCancel(ctx)
-
-	err := sub.Receive(cctx, func(ctx context.Context, msg *ps.Message) {
-		ch <- msg.Data
+	err := sub.Receive(ctx, func(ctx context.Context, msg *ps.Message) {
+		ch <- msg
 		msg.Ack()
 	})
+
 	if err != nil {
-		cancel()
 		panic(err)
 	}
 
 }
 
-func main() {
-	p := flag.String("project", "", "Google Cloud Pub Sub Project")
-	subscriptionString := flag.String("subscriptions", "", "Subscription Names")
-	flag.Parse()
-	proj = *p
-	subscriptions := strings.Split(*subscriptionString, " ")
+type subscriptions []string
 
-	ch := make(chan []byte)
-	for _, subscription := range subscriptions {
-		go subscribe(subscription, ch)
+func (s *subscriptions) String() string {
+	return strings.Join(*s, " ")
+}
+
+func (s *subscriptions) Set(value string) error {
+	for _, v := range strings.Split(value, " ") {
+		if v != "" {
+			s.appendIfMissing(strings.ToLower(v))
+		}
+	}
+	return nil
+}
+
+func (s *subscriptions) appendIfMissing(value string) {
+	for _, existing := range *s {
+		if existing == value {
+			return
+		}
+	}
+
+	*s = append(*s, value)
+}
+
+func execute() int {
+	var (
+		subs   subscriptions
+		secret string
+	)
+
+	flags := flag.NewFlagSet("event-framework-subscriber", flag.ExitOnError)
+	flags.Usage = usage
+	flags.StringVar(&proj, "project", "", "Google Cloud Pub Sub Project Name")
+	flags.Var(&subs, "subscriptions", "Subscription Names separated by space")
+	flags.StringVar(&secret, "credentials-file", "", "Google Secret json file location")
+
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		flags.Usage()
+		return 1
+	}
+
+	if proj == "" && len(subs) == 0 {
+		flags.Usage()
+		return 1
+	}
+
+	if secret != "" && os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
+		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", secret)
+	}
+
+	ch := make(chan *ps.Message)
+	ctx := context.Background()
+	for _, subscription := range subs {
+		go subscribe(ctx, subscription, ch)
 	}
 
 	for {
-		a := <-ch
-		fmt.Println(string(a))
+		select {
+		case a := <-ch:
+			data := a.Data
+			ins := []byte(fmt.Sprintf(`, "published_at":"%s"}`, a.PublishTime.Format(time.RFC3339)))
+			closingBraceIdx := bytes.LastIndexByte(data, '}')
+			data = append(data[:closingBraceIdx], ins...)
+			fmt.Println(string(data))
+		}
 	}
-
+	return 0
 }
+
+func main() {
+	os.Exit(execute())
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, helpText)
+}
+
+const helpText = `
+KW Event Framework Subscriber, subscribe google pubsubs events
+
+Usage: event-framework-subscriber -project=value -subscriptions=values -credentials-file=value
+
+Options:
+
+	-project=""            	Google Cloud Pub Sub Project Name
+	-subscriptions=""		Subscription Names separated by space
+	-credentials-file="" 	Google Secret Credentials json file location
+`
