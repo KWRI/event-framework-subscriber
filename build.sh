08cafea9865e045ef841423dfc6c1b05da17997f
Nurcahyo al Hidayah <2light.hidayah@gmail.com>
2018-11-06 04:13:45 +0700

    Reduce compiled bin using ldfsflag

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gox -output="releases/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="darwin/amd64 linux/amd64 linux/386 linux/arm64 linux/arm windows/386 windows/amd64"
+gox -ldflags="-s -w" -output="release/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="darwin/amd64 linux/amd64 linux/386 linux/arm64 linux/arm windows/386 windows/amd64"
